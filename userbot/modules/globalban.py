234505d110c4d60b176bcadcb100c79d52fd8aa0
mrismanaziz <mrismanaziz@gmail.com>
2021-10-18 17:19:40 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/globalban.py
+++ userbot/modules/globalban.py
@@ -163,7 +163,7 @@ async def gban(event):
         )
 
     if BOTLOG and count != 0:
-        reply = await event.get_reply_message()
+        await event.get_reply_message()
         if reason:
             await event.client.send_message(
                 BOTLOG_CHATID,
