8a99b1e391968b5ce02ce0bac9e2d5fbeeb8fba1
mrismanaziz <mrismanaziz@gmail.com>
2021-10-18 20:53:29 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/markdown.py
+++ userbot/modules/markdown.py
@@ -7,7 +7,6 @@ from random import choice
 
 from telethon import events
 from telethon.extensions.markdown import DEFAULT_URL_RE
-from telethon.tl import types
 from telethon.tl.functions.messages import EditMessageRequest
 from telethon.tl.types import (
     MessageEntityBold,
