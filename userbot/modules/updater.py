37a03b04c50b043f92a76434c6673d8d935d89f5
zxcskyy <92031643+zxcskyy@users.noreply.github.com>
2021-10-21 00:26:21 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -202,9 +202,9 @@ CMD_HELP.update(
     {
         "update": f"**Plugin : **`update`\
         \n\n  •  **Syntax :** `{cmd}update`\
-        \n  •  **Function : **Untuk Melihat Pembaruan Terbaru Man-Userbot.\
+        \n  •  **Function : **Untuk Melihat Pembaruan Terbaru.\
         \n\n  •  **Syntax :** `{cmd}update deploy`\
-        \n  •  **Function : **Untuk MengUpdate Fitur Terbaru Dari Man-Userbot.\
+        \n  •  **Function : **Untuk MengUpdate Fitur Terbaru.\
     "
     }
 )
